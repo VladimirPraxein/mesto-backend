b88d75b99921e475e9da65fc892367f33707ea2b
VladimirPraxein <MrVova2311@yandex.ru>
2023-03-31 22:15:45 +0400

    add env

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('./middlewares/cors');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 
@@ -19,26 +18,23 @@ const auth = require('./middlewares/auth');
 const NotFound = require('./errors/notFound');
 
 const { PORT = 3000 } = process.env;
-const app = express();
-mongoose.set('strictQuery', true);
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
-
-app.use(cors);
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 });
+mongoose.set('strictQuery', true);
 
+const app = express();
+app.use(cors);
 app.use(limiter);
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors);
 
 mongoose
   .connect(URL)
